98e27fec544e5b3bbe36a413f7592ceb4497119d
Zekai "Zk" Zhang <94256529+KaiserZZK@users.noreply.github.com>
2022-08-04 15:12:08 -0400

    Add files via upload

---
+++ spaCy-custom-NER/spaCy-custom-NER_data/a_gathering_v1/char_counter.py
@@ -0,0 +1,9 @@
+str="NYC AREA MISSING DOG ALERT: Bella is lost at 71st street and Broadway in Guttenberg, NJ. She's chipped: 985141004188779. If you've seen or found her please call 888-466-3242 and PLEASE RT BELLA!"
+i=0
+
+for char in str:
+    print(f'{i}th character is {char}')
+    i+=1
+
+print(str[45:56])
+print(str[61:69])
